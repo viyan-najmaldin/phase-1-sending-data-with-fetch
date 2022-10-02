3b262cc75c48414642a7ba30de3559fe107d68e2
viyan-najmaldin <najmadinviyan@gmail.com>
2022-10-03 00:02:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+const body = document.querySelector("body");
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json", Accept: "application/json" },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => (document.body.innerHTML = data["id"]))
+    .catch((err) => {
+      const error = document.createElement("div");
+      error.innerHTML = "Unauthorized Access";
+      body.append(error);
+    });
+}
